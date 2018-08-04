7f7f59ac8dc67c376d6373a55958a15f46000fe1
Jeppe Smith <mail@jeppesmith.dk>
2018-08-04 14:23:01 +0200

    add check for addListener

--- content-script.js
+++ content-script.js
@@ -1,9 +1,14 @@
-// Listen on messages from the background-script.
-chrome.runtime.onMessage.addListener(action => {
-  const { type } = action
-
-  // Reload the page when told so.
-  if (type === 'RELOAD') {
-    window.location.reload()
-  }
-})
+// Sometimes content scripts are run more than once and when we try adding
+// listeners at the top of the script that is run at document_start the function
+// might not be available yet.
+if (typeof chrome.runtime.onMessage.addListener === 'function') {
+  // Listen on messages from the background-script.
+  chrome.runtime.onMessage.addListener(action => {
+    const { type } = action
+  
+    // Reload the page when told so.
+    if (type === 'RELOAD') {
+      window.location.reload()
+    }
+  })
+}
